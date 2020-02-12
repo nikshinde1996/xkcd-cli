28fc70a82455a434ec435f1eb96ab378e73ed02d
nikshinde1996 <crenickshinde1996@gmail.com>
2020-02-12 23:52:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xkci-cli',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        xkcd=src.main:cli
+    ''',
+)
